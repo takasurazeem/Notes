aff39d098c933a63a6a805fa544af90f8f8d3ff2
Takasur Azeem <takasurazeem@gmail.com>
2024-02-18 01:06:02 +0500

    Update Note page

--- Notes/Views/NotePage.xaml.cs
+++ Notes/Views/NotePage.xaml.cs
@@ -2,14 +2,30 @@ namespace Notes.Views;
 
 public partial class NotePage : ContentPage
 {
-    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+    readonly string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
 
     public NotePage()
     {
         InitializeComponent();
 
-        if (File.Exists(_fileName))
-            TextEditor.Text = File.ReadAllText(_fileName);
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
+    }
+
+    private void LoadNote(string fileName)
+    {
+        Models.Note noteModel = new Models.Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
     }
 
     private void SaveButton_Clicked(object sender, EventArgs e)
